83dfcb143d7fc9de53d4882f695a467fe238772e
praisy <praisyprasad123@gmail.com>
2021-05-14 19:23:45 -0500

    frountendsection

---
+++ frountendsection/notes.py
@@ -0,0 +1,22 @@
+#html(for displaying contents)
+#css(cascading style sheet)
+#bootstrap(advanced styling)
+#javascript
+#ajax(angular)
+#html <tags>
+#<html>
+#<head>
+#<title>this is title</title>
+#</head>
+#<body>
+
+
+
+#</body>
+
+
+
+
+
+
+#</html>
